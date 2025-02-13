5b7c2d0815ebf92a88690d5668a406e21dc259c5
CristhianHatzman <cristhianhatzman@gmail.com>
2025-02-13 18:43:49 -0300

    add: Variáveis e funções do JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,15 @@
+// Comentários em JS
+ /*Comentários em
+ bloco com
+ JS*/
+
+ const nome = "Cristhian"
+ let Nome 
+ var cor = "Azul"
+ var cor = "Amarelo"
+ let cidade = "Registro"
+ cidade = "Jacupiranga"
+ 
+const message = "Hello World! Iniciando estudos com JavaSript!"
+ console.log(message)
+
